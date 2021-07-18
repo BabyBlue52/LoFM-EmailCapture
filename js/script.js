4e3558bc060c42e327b080bba9180a65b9a658f4
Chris Hennemann <chris.hennemann256@gmail.com>
2021-07-18 16:48:45 -0400

    Convert to JQuery

---
+++ js/script.js
@@ -0,0 +1,40 @@
+import $ from "jquery";
+
+
+$(document).ready(function(){
+  console.log('toasted');  
+    $('.submitForm').click(function() {
+      var val = document.getElementById("email");
+      var url = ""
+      
+      
+      toasty();
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr
+        .send(
+          JSON.stringify({
+            val: val
+          })
+        )
+    });
+
+    var delay;
+
+    function toasty() {
+      delay = setTimeout(openNotification(), 750);
+      delay = setTimeout(closeNotification(), 4000);
+    }
+
+    const openNotification = () => {
+      console.log('open')
+      $("#toast-wrapper").animate({ bottom: "300" }, 500);
+    }
+
+    const closeNotification = () => {
+      console.log('close')
+    }
+});
+   
