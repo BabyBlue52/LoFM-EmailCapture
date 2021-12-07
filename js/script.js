36b034bad9eaaab20267ee8288d4826eb82d25fb
BabyBlue52 <chris.hennemann256@gmail.com>
2021-12-06 21:42:11 -0500

    Add favicon, add netlify form

--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
-<<<<<<< CodeSandbox
-
-
 $(document).ready(function () {
-  console.log("toasted");
-  $(".submitForm").click(function () {
-    var val = document.getElementById("email");
-    var url = "";
-
-    toasty();
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(
-      JSON.stringify({
-        val: val
-      })
-    );
-  });
-
-  function toasty() {
-    delay = setTimeout(openNotification(), 750);
-    delay = setTimeout(closeNotification(), 4000);
-  }
+  const btn = document.getElementById('#toast');
 
   const openNotification = () => {
     console.log("open");
@@ -32,4 +9,16 @@ $(document).ready(function () {
   const closeNotification = () => {
     console.log("close");
   };
+
+  console.log("toasted");
+  
+  btn.click(function() {
+    toasty();
+  })
+  
+  function toasty() {
+    delay = setTimeout(openNotification(), 750);
+    delay = setTimeout(closeNotification(), 4000);
+  }
+ 
 });
